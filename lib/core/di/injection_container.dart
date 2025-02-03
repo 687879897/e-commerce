27f9db3dc7ff0a85320f85773c301328155398b0
Your Name <your.email@example.com>
2025-02-03 23:13:05 +0200

    finish model files and bloc login

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -3,6 +3,9 @@
 import 'package:asroo_store/core/app/app_cubit/app_cubit.dart';
 import 'package:get_it/get_it.dart';
 
+import '../serviec/graphql/api_service.dart';
+import '../serviec/graphql/dio_factory.dart';
+
 final sl = GetIt.instance;
 
 Future<void> setupInjector() async {
@@ -10,7 +13,10 @@ Future<void> setupInjector() async {
 }
 
 Future<void> _initCore() async {
+  final dio = DioFactory.getDio();
+
   sl.registerCachedFactory(AppCubit.new);
+  sl.registerLazySingleton<ApiService>(() => ApiService(dio));
 
 
 }
---
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -0,0 +1,36 @@
+import 'package:asroo_store/core/language/lang_keys.dart';
+import 'package:asroo_store/features/auth/data/data_source/auth_data_source.dart';
+import 'package:asroo_store/features/auth/data/models/login_request_body.dart';
+import 'package:asroo_store/features/auth/data/models/login_response.dart';
+
+import 'package:asroo_store/features/auth/data/models/user_role_response.dart';
+
+import '../../../../core/serviec/graphql/api_result.dart';
+
+class AuthRepos {
+  const AuthRepos(this._dataSource);
+  final AuthDataSource _dataSource;
+
+  //Loign
+  Future<ApiResult<LoginResponse>> login(LoginRequestBody body) async {
+    try {
+      final response = await _dataSource.login(body: body);
+
+      return ApiResult.success(response);
+    } catch (error) {
+      return const ApiResult.failure(LangKeys.loggedError);
+    }
+  }
+
+  //Get User Role
+  Future<UserRoleResponse> userRole(String token) async {
+    final response = await _dataSource.userRole(token);
+    return response;
+  }
+
+  //SignUp
+
+
+  // add user id in firebase to used with notfication data base
+
+}
